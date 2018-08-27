910e9aa16a53fb6407b7fca2102b31d480440106
teaho2015 <teaho2015@gmail.com>
2018-08-27 17:56:02 +0800

    [add]config, and update db init.

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ var autoRoutes = require('express-auto-routes');
 var CORS = require('./middlewares/CORS');
 var simpleLogger = require('./middlewares/simpleLogger');
 var simpleUserSession = require('./middlewares/simpleUserSession');
+var mongoose = require('mongoose');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// init database
+global.db = mongoose.createConnection(config.db);
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -42,7 +47,7 @@ routes(path.join(__dirname, './controllers'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+	next(createError(404));
 });
 
 // app.use(function(req, res, next) {
@@ -52,15 +57,15 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // console.error(err);
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// console.error(err);
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    // res.render('error');
-    // if (err.status) res.status(err.status);
+	// render the error page
+	res.status(err.status || 500);
+	// res.render('error');
+	// if (err.status) res.status(err.status);
 
 	res.json({
 		code: err.code || 0,
